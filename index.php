6deddccadd71aa443e13988077153dfc8a08696b
FaizzaSafaMaghribi <107838787+FaizzaSafaMaghribi@users.noreply.github.com>
2022-06-21 07:33:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="CSS\indexstyle.css">
+<style>
+</style>
+</head>
+<body>	
+  	<input class="menu-icon" type="checkbox" id="menu-icon" name="menu-icon"/>
+  	<label for="menu-icon"></label>
+  	<nav class="nav"> 		
+  		<ul class="pt-5">
+  			<li><a href="SL.php">Straight Line</a></li>
+  			<li><a href="RB.php">Reducing Balance</a></li>
+  			<li><a href="SOY.php">Sum of The Year</a></li>
+  			<li><a href="UOA.php">Unit of Activity</a></li>
+  		</ul>
+  	</nav>
+
+  	<div class="section-center">
+  		<h1 class="mb-0">Depreciation</h1>
+  	</div>
+</body>
+</html>
